49023a4ad9fe0ffeb9f2a8e581a2fbd1a7b81324
Desno365 <desno365@gmail.com>
2016-02-21 11:22:47 +0100

    r012 release candidate

--- DesnoGuns_Mod_by_Desno365.js
+++ DesnoGuns_Mod_by_Desno365.js
@@ -19,7 +19,7 @@ const DEBUG2 = false;
 const TAG = "DesnoGuns";
 
 // updates variables
-const CURRENT_VERSION = "r011";
+const CURRENT_VERSION = "r012";
 var latestVersion;
 
 //activity and other Android variables
