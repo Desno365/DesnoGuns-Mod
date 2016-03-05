79a50901d6ec4bd0147f8deb585be25de72a28fc
Desno365 <desno365@gmail.com>
2016-03-05 23:15:18 +0100

    Automatic modpkg creation (libraries automatically added in the script, no more copy and paste)

---
+++ shell/build-all-and-move.sh
@@ -0,0 +1,4 @@
+cd /Users/Desno365/Documents/Projects/MinecraftMods/DesnoGuns-Mod/DesnoGuns-Mod/shell
+sh build-js.sh
+sh build-modpkg.sh
+sh move-on-sdcard.sh
---
+++ shell/build-modpkg.sh
@@ -0,0 +1,4 @@
+cd /Users/Desno365/Documents/Projects/MinecraftMods/DesnoGuns-Mod/DesnoGuns-Mod/modpkg
+rm build/DesnoGuns_Mod_r000_Desno365.modpkg
+zip -rq build/DesnoGuns_Mod_r000_Desno365.modpkg images
+zip -rq build/DesnoGuns_Mod_r000_Desno365.modpkg script
