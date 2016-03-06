bfeb729a0b35324eeecc1cfb216d83bf82a8e3bd
Desno365 <desno365@gmail.com>
2016-03-06 14:11:29 +0100

    Remove .DS_Store before zipping the modpkg

--- shell/build-modpkg.sh
+++ shell/build-modpkg.sh
@@ -1,4 +1,5 @@
 cd /Users/Desno365/Documents/Projects/MinecraftMods/DesnoGuns-Mod/DesnoGuns-Mod/modpkg
+find . -name '*.DS_Store' -type f -delete
 rm build/DesnoGuns_Mod_r000_Desno365.modpkg
 zip -rq build/DesnoGuns_Mod_r000_Desno365.modpkg images
 zip -rq build/DesnoGuns_Mod_r000_Desno365.modpkg script
