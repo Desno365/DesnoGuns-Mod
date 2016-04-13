3d75dc78149e18d3bb83f92d65c8ce3dc2b5b785
Desno365 <desno365@gmail.com>
2016-04-13 19:56:23 +0200

    r015 release candidate

--- src/DesnoGuns_Main.js
+++ src/DesnoGuns_Main.js
@@ -18,7 +18,7 @@ const DEBUG1 = false; // debug: getSavedBoolean
 const DEBUG2 = false; // disable base64 code
 
 // updates variables
-const CURRENT_VERSION = "r014";
+const CURRENT_VERSION = "r015";
 var latestVersion;
 
 //activity and other Android variables
