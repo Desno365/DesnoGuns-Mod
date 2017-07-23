9d3e588fd41f9c09c9192a6d79c5538521fad432
Desno365 <desno365@gmail.com>
2017-07-23 15:44:31 +0200

    Support for InventoryPlus

--- modpkg/build/shell/build-modpkg.sh
+++ modpkg/build/shell/build-modpkg.sh
@@ -2,6 +2,7 @@ cd /Users/Desno365/Documents/Projects/MinecraftMods/DesnoGuns-Mod/DesnoGuns-Mod/
 find . -name '*.DS_Store' -type f -delete
 rm build/DesnoGuns_Mod_r000_Desno365.modpkg
 zip -rq build/DesnoGuns_Mod_r000_Desno365.modpkg images
+zip -rq build/DesnoGuns_Mod_r000_Desno365.modpkg inventory_plus
 zip -rq build/DesnoGuns_Mod_r000_Desno365.modpkg script
 zip -rq build/DesnoGuns_Mod_r000_Desno365.modpkg desnoguns-sounds
 zip -rq build/DesnoGuns_Mod_r000_Desno365.modpkg README.txt
